8b28d0e344ce6239404306bd4512fe9db6b60e7b
eric <ericweix10@gmail.com>
2023-02-03 03:50:03 -0800

    Implemented part of lab1, but I don't know how to judge whether the stream is eof

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,75 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) :
+    _output(capacity), _capacity(capacity), _idx_str(), _next(0) {}
+
+size_t StreamReassembler::total_size() {
+    return unassembled_bytes() + _output.buffer_size();
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    // string _data;
+    // size_t eff_length = min(_capacity - total_size(), data.length());
+    if (data.length() == 0) return;
+    
+    map<size_t, string>::iterator it;
+
+    if (eof) {
+        _output.end_input();
+    }
+
+    // _data = data.substr(0, eff_length);
+    // cout << "here" << endl;
+    it = _idx_str.find(index);
+    if (it == _idx_str.end()) {
+        _idx_str[index] = data;
+    } else {
+        if (it->second.length() < data.length()) {
+            _idx_str[index] = data;
+        }
+    }
+
+    it = _idx_str.begin();
+    for (; it != _idx_str.end(); it++) {
+        if (it->first < _next) {
+            size_t item_length = it->second.length();
+            if (it->first + item_length <= _next) {
+                continue;
+            } else {
+                _next += _output.write(it->second.substr(_next - it->first));
+            }
+        } else if (it->first == _next) {
+            _next += _output.write(it->second);
+        } else {
+            break;
+        }
+    }
+    _idx_str.erase(_idx_str.begin(), it);
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    size_t next = _next;
+    size_t unassembled = 0;
+    for (auto it: _idx_str) {
+        if (it.first < next) {
+            if (it.first + it.second.length() <= next) {
+                continue;
+            } else {
+                unassembled += (it.first + it.second.length() - next);
+                next = it.first + it.second.length();
+            }
+        } else {
+            unassembled += it.second.length();
+            next = it.first + it.second.length();
+        }
+    }
+    return unassembled; 
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { 
+    return _output.buffer_empty() && _idx_str.empty(); 
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 #include <string>
+#include <map>
+#include <iostream>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +16,10 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    map<size_t, string> _idx_str;
+    size_t _next;
+
+    size_t total_size();
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
