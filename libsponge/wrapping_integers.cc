0d78c8927c529ce14718691e774d270761d620de
eric <ericweix10@gmail.com>
2023-02-05 22:08:02 -0800

    Implement wrapping helper functions

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#define int32_mask 0xffffffff
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +15,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + n;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,27 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t _n = n.raw_value();
+    uint64_t _isn = isn.raw_value();
+    uint64_t first32 = checkpoint & 0xffffffff00000000;
+    uint64_t last32;
+    uint64_t res;
+    uint64_t aux = 0x0000000100000000; // 1 << 32
+    if (int64_t(_n) - int64_t(_isn) >= 0) {
+        last32 = uint64_t(_n - _isn);
+    } else {
+        last32 = uint64_t((_n + aux) - _isn);
+    }
+    if (last32 > checkpoint) {
+        return last32;
+    } else {
+        res = first32 | last32;
+        if (res == checkpoint) {
+            return res;
+        } else if (res < checkpoint) {
+            return ((res + aux) - checkpoint) > (checkpoint - res)? res: (res + aux);
+        } else {
+            return (res - checkpoint) > (checkpoint - (res - aux))? (res - aux): res;
+        }
+    }
 }
